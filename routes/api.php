b465d3d0c1ab70a09a62f2ef5c11ba6d65f8ce11
mounkaila <mounkaila144@gmail.com>
2022-09-07 15:59:12 +0100

    Ajoute de l'Authentification et du Register Api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategorieController;
 
@@ -20,6 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 
 Route::middleware('api')->group(function () {
